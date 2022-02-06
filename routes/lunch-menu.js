f34adff4b08c88c3db86ef1bf148a735ef8f79d4
yoelgal <yoelgal108@gmail.com>
2022-02-06 18:17:10 +0000

    week in backend

--- routes/lunch-menu.js
+++ routes/lunch-menu.js
@@ -4,7 +4,10 @@ const dayjs = require('dayjs');
 const lunchMenu = require('../utils/lunch-menu.json')
 
 router.get('/', async (req,res)=>{
-    res.json(lunchMenu)
+    res.json({
+        week:1,
+        lunchMenu
+    })
 })
 
 module.exports = router
