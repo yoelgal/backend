30306eaf4b26d94f19922e84d6a4961b12bbca6a
yoelgal <yoelgal108@gmail.com>
2022-02-18 13:05:47 +0000

    changes

--- routes/news.js
+++ routes/news.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const needle = require('needle')
 
 router.get('/', async (req, res) => {
-    const newsRes = await needle(`https://newsapi.org/v2/top-headlines?&apiKey=4db889f4ad6242d5832b52a3c5a53f15`)
+    const newsRes = await needle(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=4db889f4ad6242d5832b52a3c5a53f15`)
     const newsData = newsRes.body.articles.map(e => ({
         title: e.title, source: e.source.name
     }))
